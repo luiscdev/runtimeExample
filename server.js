6ed840892e214954de56b30d9c53a16a23d69d10
Luis Zuleta <luiszuletacm@gmail.com>
2024-01-24 14:16:43 -0500

    delete axios

--- server.js
+++ server.js
@@ -7,16 +7,12 @@ require('dotenv').config();
 
 app.get("/saludos", function(req, res){
     res.send("Hola desde express");
-    console.log("aa")
     logsGenerator()
 });
 
 async function logsGenerator() {
     for (let index = 0; index < 300; index++) {
-        const response = await axios.get(`https://api.api-ninjas.com/v1/loremipsum?paragraphs=1`, {headers: {
-            'X-Api-Key': 'EeZsr9Wb0zBL5NhDtUNi7g==aEV6gjAN5J5jmcZb',
-        }})
-        console.log(response.data)
+        console.log("Lorem ipsum leo vel orci porta non pulvinar neque. Porta nibh venenatis cras sed felis eget velit aliquet sagittis. In aliquam sem fringilla ut morbi tincidunt augue interdum velit. Consectetur libero id faucibus nisl tincidunt eget. Morbi tristique senectus et netus. Vitae elementum curabitur vitae nunc sed velit dignissim sodales. Sit amet nulla facilisi morbi tempus iaculis urna. Suscipit tellus mauris a diam maecenas. In aliquam sem fringilla ut. Tortor at risus viverra adipiscing. Scelerisque eleifend donec pretium vulputate sapien nec. Id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. Mi tempus imperdiet nulla malesuada pellentesque elit. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Nam at lectus urna duis convallis.\n")
     }
 }
 
